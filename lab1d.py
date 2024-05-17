d2812ff4f7601f1e0c7dbc3284eca93e9806c856
Megodwin Varghese <mvarghese10@myseneca.ca>
2024-05-17 00:40:04 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Description: This program will output the math of 10 + 2 * 5 = 20.'''
+
+
+x = 10
+y = 2
+z = 5
+
+
+sum = x + y * z
+
+
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(sum))
