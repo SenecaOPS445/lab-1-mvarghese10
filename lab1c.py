29e8420e88be4cf3a2bb5c56f5090108b7533bc1
Megodwin Varghese <mvarghese10@myseneca.ca>
2024-05-17 00:16:24 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program will output the age of a person, with the value of provided objects.'''
+
+
+name = 'Isaac'
+age = 72
+
+
+
+print(name + " is " + str(age) + " years old!")
