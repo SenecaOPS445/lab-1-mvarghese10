ec2242a0505413a8da394f264bcf65dc8f76941c
Megodwin Varghese <mvarghese10@myseneca.ca>
2024-05-16 23:47:34 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+'''Description: This program will output "How old are you Isaac?" to the screen, with "Isaac" being the value of an object called "name".'''
+
+
+name = 'Isaac'
+print('How old are you ' + name + '?')
